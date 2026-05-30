085a0638e7f652c80edffd3d59c21c85ec2ceef8
Tempunus <tempunus@gmail.com>
2026-05-30 11:12:49 -0300

    Adicionar suporte PWA instalavel

--- gcondid/urls.py
+++ gcondid/urls.py
@@ -1,28 +1,17 @@
-"""
+﻿"""
 URL configuration for gcondid project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from users.views import CustomLoginView, SignUpView
 
 urlpatterns = [
+    path('service-worker.js', TemplateView.as_view(template_name='pwa/service-worker.js', content_type='application/javascript'), name='service_worker'),
     path('', include('dashboard.urls')),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
---
+++ static/js/pwa.js
@@ -0,0 +1,24 @@
+﻿let deferredInstallPrompt = null;
+const installButton = document.querySelector("[data-install-app]");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  deferredInstallPrompt = event;
+  if (installButton) installButton.hidden = false;
+});
+
+if (installButton) {
+  installButton.addEventListener("click", async () => {
+    if (!deferredInstallPrompt) return;
+    deferredInstallPrompt.prompt();
+    await deferredInstallPrompt.userChoice;
+    deferredInstallPrompt = null;
+    installButton.hidden = true;
+  });
+}
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").catch(() => {});
+  });
+}
